3d3a828c349fde0bc5024eff7b788384af1c4c7f
jaimepr01 <91058573+jaimepr01@users.noreply.github.com>
2021-09-22 12:08:40 +0200

    HolaMundoJava2

---
+++ HolaMundo2.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jaime
+ */
+
+public class HolaMundo2 {
+    public static void main (String[] args) {
+    String miVar = "Hola alumnos";
+    System.out.println("miVar = " + miVar);
+        System.out.println(miVar);
+        
+        String texto2 = " de CEEP";
+        
+        System.out.println(miVar + texto2);
+    
+}
+}
