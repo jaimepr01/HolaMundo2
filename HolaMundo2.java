8bcf44c657df151d2bd1e495cfaf9deeada90013
jaimepr01 <91058573+jaimepr01@users.noreply.github.com>
2021-09-23 09:41:16 +0200

    Update HolaMundo2.java

--- HolaMundo2.java
+++ HolaMundo2.java
@@ -8,16 +8,20 @@
  *
  * @author jaime
  */
-
 public class HolaMundo2 {
-    public static void main (String[] args) {
-    String miVar = "Hola alumnos";
-    System.out.println("miVar = " + miVar);
-        System.out.println(miVar);
+        public  static  void  main ( String [] args ) {
+        String miVar =  " Hola alumnos " ;
+        System.out.println ( " miVar = "  + miVar);
+        System.out.println (miVar);
         
-        String texto2 = " de CEEP";
+        String texto2 =  "de CEEP" ;
         
-        System.out.println(miVar + texto2);
-    
+        System.out.println (miVar + texto2);
+           
+        System.out.println("Salto de linea: \n" + texto2);
+        System.out.println("Tabulador: \t" + texto2);
+        System.out.println("Retroceso: \b" + texto2);
+        System.out.println("Comilla simple:" + 'c');
+        System.out.println("Comilla doble:" + "CEEP");
 }
 }
